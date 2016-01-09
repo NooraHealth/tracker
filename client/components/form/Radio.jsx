4424713659b08cbcc10dd2575953e30704e3e7eb
lucyrichards <lucyannerichards@gmail.com>
2016-01-08 17:25:36 -0800

    added key to each radio option in Radiogroup

--- client/components/form/Radio.jsx
+++ client/components/form/Radio.jsx
@@ -24,6 +24,7 @@ class RadioGroup extends BaseComponent {
       } 
 
       return < Form.RadioGroup.Radio
+        key={ ref }
         option={ option }
         ref={ ref }
         checked= { checked }
