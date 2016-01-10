8bfc6bbffb09eb7d24bb2bc7a7077449666555c7
lucyrichards <lucyannerichards@gmail.com>
2016-01-09 16:38:51 -0800

    valueLink to phone value in NewPatientPage component is working

--- client/components/NewPatient/NewPatientPage.jsx
+++ client/components/NewPatient/NewPatientPage.jsx
@@ -1,31 +1,92 @@
 
-class NewPatientPage extends BaseComponent {
-  constructor( props ){
-    super(props);
-    this.languageOptions = [
-      { title: 'Kannada', value: 'kannada' },
-      { title: 'Hindi', value: 'hindi' },
-      { title: 'English', value: 'english' }
-    ];
-    this._bind('onSubmit');
-  }
+var NewPatientPage = React.createClass({
+
+  mixins: [LinkedStateMixin],
+
+  getDefaultProps() {
+    return {
+      languageOptions: [
+        { title: 'Kannada', value: 'kannada' },
+        { title: 'Hindi', value: 'hindi' },
+        { title: 'English', value: 'english' }
+      ],
+      phoneLength: 10,
+    };
+  },
+
+
+  getInitialState() {
+    date = moment().toDate() 
+    return {
+      phone: '',
+      language: 'kannada',
+      hospital: 'jayadeva',
+      subscribeToIVR: false
+    };
+  },
 
   onSubmit(){
     console.log("A new patient was submitted");
     console.log(this);
-  }
+    var phone = this.state.phone;
+    var language = this.state.language;
+    var subscribeToIVR = this.state.subscribeToIVR;
+    var date = this.state.date;
+    var hospital = this.state.hospital;
+
+    if( phone.length != this.props.phoneLength )
+      swal("Error", "Patient phone must be 10 digits", "error");
+    else {
+      var patient = {
+        phone: phone,
+        language: language,
+        subscribes_to_ivr: subscribeToIVR,
+        has_been_input_to_ivr_system: false,
+        date_added: date,
+        date_discharged: null,
+        date_first_class: null,
+        date_practical: null,
+        hospital: hospital
+      };
+
+      Meteor.call( "insertPatient", patient);
+
+      analytics.track( "Action", {
+        location: "newPatient",
+        text: "submit",
+        color: "blue"
+      });
+
+      FlowRouter.go("/");
+    }
+  },
     
   render() {
+    console.log("Rerendering the new patient form page: ", this.state);
     return (
       <div>
         <Form onSubmit={ this.onSubmit } >
-          <Form.Input type='number' icon='icon icon-form-tel' name='phone' ref={ (i)=> this.phoneInput = i} />
-          <Form.RadioGroup ref={ (i)=> this.languageSelect = i } options={ this.languageOptions } value='kannada'/>
-          <Form.Checkbox title='Subscribe to Phone Messages' ref={ (i)=> this.subscribeCheckbox = i }/>
+          <Form.Input 
+            type='number' 
+            icon='icon icon-form-tel'
+            maxLength={ this.phoneLength } 
+            ref={ (i)=> this.phoneInput = i} 
+            valueLink={ this.linkState('phone') }
+          />
+          <Form.RadioGroup
+            ref={ (i)=> this.languageSelect = i }
+            options={ this.props.languageOptions }
+            //linkValue={ this.linkState('language') }
+          />
+          <Form.Checkbox
+            title='Subscribe to Phone Messages'
+            ref={ (i)=> this.subscribeCheckbox = i }
+            //valueLink={ this.linkState('subscribeToIVR') }
+          />
         </Form>
       </div>
     )
   }
-}
+});
 
 this.NewPatientPage = NewPatientPage
--- client/components/form/Input.jsx
+++ client/components/form/Input.jsx
@@ -1,26 +1,16 @@
 class Input extends BaseComponent {
 
-  constructor( props ){
-    super(props);
-    this.state = {
-      value: ""
-    }
-    this._bind('handleChange');
-  }
-
-  handleChange( event ){
-    return this.setState({ value: event.target.value.substr(0, 10) });
-  }
-
   render(){
-    var { title, icon, ...inputProps } = this.props;
-    var value = this.state.value;
+    console.log(this.props);
+    var { title, icon, valueLink, ...inputProps } = this.props;
+    console.log(valueLink);
+    console.log("Reredning input");
     return (
       <div className="item-content">
         <div className="item-media"><i className={ icon }></i></div>
         <div className="item-inner">
           <div className="item-input">
-            <input { ...inputProps } value={ value } onChange={ this.handleChange } />
+            <input { ...inputProps } valueLink={ valueLink } />
           </div>
         </div>
       </div>
@@ -31,6 +21,7 @@ class Input extends BaseComponent {
 Input.propTypes = {
   value: React.PropTypes.string,
   icon: React.PropTypes.string,
+  valueLink: React.PropTypes.func
 }
 
 this.Form.Input = Input;
